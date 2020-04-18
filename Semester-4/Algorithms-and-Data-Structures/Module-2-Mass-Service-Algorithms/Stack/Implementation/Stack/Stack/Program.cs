1b641ba7fa6fbe486daefb4a02886a0f30a0fdeb
Mikołaj Buczak <mikolaj.buczak@gmail.com>
2020-04-18 23:00:56 +0200

    Added an implementation of a Stack

---
+++ Semester-4/Algorithms-and-Data-Structures/Module-2-Mass-Service-Algorithms/Stack/Implementation/Stack/Stack/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Stack
+{
+    class Program
+    {
+        static void Main()
+        {
+            var stack = new Stack<int>();
+
+            stack.Push(5);
+
+            System.Console.WriteLine(stack.IsEmpty());
+            System.Console.WriteLine(stack.Peek());
+            stack.Push(2);
+            System.Console.WriteLine(stack.Pop());
+            System.Console.WriteLine(stack.Peek());
+            System.Console.WriteLine(stack.IsEmpty());
+            System.Console.WriteLine(stack.Pop());
+            System.Console.WriteLine(stack.IsEmpty());
+
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ Semester-4/Algorithms-and-Data-Structures/Module-2-Mass-Service-Algorithms/Stack/Implementation/Stack/Stack/Stack.cs
@@ -0,0 +1,65 @@
+﻿namespace Stack
+{
+    class Stack<T>
+    {
+        private readonly static int MAX_SIZE = 1000;
+        private readonly T[] data = new T [MAX_SIZE];
+        private int numberOfElements = 0;
+
+        public Stack()
+        {
+            for (int i = 0; i < MAX_SIZE; i++)
+                data[i] = default;
+        }
+
+        public bool IsEmpty()
+        {
+            return numberOfElements == 0;
+        }
+
+        public int Size()
+        {
+            return numberOfElements;
+        }
+
+        public void Push(T value)
+        {
+            try
+            {
+                data[numberOfElements] = value;
+                numberOfElements++;
+            }
+            catch (System.IndexOutOfRangeException)
+            {
+                throw new System.IndexOutOfRangeException("Stack full.");
+            }
+        }
+
+        public T Pop()
+        {
+            try
+            {
+                T returnValue = data[numberOfElements - 1];
+                data[numberOfElements - 1] = default;
+                numberOfElements--;
+                return returnValue;
+            }
+            catch (System.IndexOutOfRangeException)
+            {
+                throw new System.IndexOutOfRangeException("Stack empty.");
+            }
+        }
+
+        public T Peek()
+        {
+            try
+            {
+                return data[numberOfElements - 1];
+            }
+            catch (System.IndexOutOfRangeException)
+            {
+                throw new System.IndexOutOfRangeException("Stack empty.");
+            }
+        }
+    }
+}
