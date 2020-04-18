4a7367bbbcd16526db036f357c3116e74bcc4aef
Mikołaj Buczak <mikolaj.buczak@gmail.com>
2020-04-18 22:45:49 +0200

    Added an implementation of Queue

---
+++ Semester-4/Algorithms-and-Data-Structures/Module-2-Mass-Service-Algorithms/Queue/Implementation/Queue/Queue/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Queue
+{
+    class Program
+    {
+        static void Main()
+        {
+            var queue = new Queue<int>();
+
+            queue.Enqueue(5);
+
+            System.Console.WriteLine(queue.IsEmpty());
+            System.Console.WriteLine(queue.Peek());
+            queue.Enqueue(2);
+            System.Console.WriteLine(queue.Dequeue());
+            System.Console.WriteLine(queue.Peek());
+            System.Console.WriteLine(queue.IsEmpty());
+            System.Console.WriteLine(queue.Dequeue());
+
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ Semester-4/Algorithms-and-Data-Structures/Module-2-Mass-Service-Algorithms/Queue/Implementation/Queue/Queue/Queue.cs
@@ -0,0 +1,71 @@
+﻿namespace Queue
+{
+    class Queue <T>
+    {
+        private readonly static int MAX_SIZE = 1000;
+        private readonly T[] data = new T [MAX_SIZE];
+        private int numberOfElements = 0;
+
+        public Queue()
+        {
+            for (int i = 0; i < MAX_SIZE; i++)
+                data[i] = default;
+        }
+
+        public bool IsEmpty()
+        {
+            return numberOfElements == 0;
+        }
+
+        public int Size()
+        {
+            return numberOfElements;
+        }
+
+        public void Enqueue(T value)
+        {
+            try
+            {
+                data[numberOfElements] = value;
+                numberOfElements++;
+            }
+            catch (System.IndexOutOfRangeException)
+            {
+                throw new System.IndexOutOfRangeException("Queue full.");
+            }
+        }
+
+        public T Dequeue()
+        {
+            try
+            {
+                T first = data[0];
+
+                for (int i = 0; i < numberOfElements; i++)
+                    data[i] = data[i + 1];
+
+                numberOfElements--;
+
+                data[numberOfElements] = default;
+
+                return first;
+            }
+            catch(System.IndexOutOfRangeException)
+            {
+                throw new System.IndexOutOfRangeException("Queue empty.");
+            }
+        }
+
+        public T Peek()
+        {
+            try
+            { 
+                return data[0];
+            }
+            catch(System.IndexOutOfRangeException)
+            {
+                throw new System.IndexOutOfRangeException("Queue empty.");
+            }
+        }
+    }
+}
