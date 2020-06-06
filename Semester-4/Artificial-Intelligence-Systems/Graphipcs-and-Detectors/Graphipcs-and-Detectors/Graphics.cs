2036fade8c6ee5ab74528af5ecf58ec318b204ad
Mikołaj Buczak <spam.mikolaj.buczak@gmail.com>
2020-06-06 12:23:09 +0200

    Added a covering letter (english), added emty c# project to AIS about graphics, chosen topics for modules 2-4 (ISM)

---
+++ Semester-4/Artificial-Intelligence-Systems/Graphipcs-and-Detectors/Graphipcs-and-Detectors/Graphics.cs
@@ -0,0 +1,24 @@
+﻿namespace Graphipcs_and_Detectors
+{
+    using System.Drawing;
+    static class Graphics
+    {
+        public static void GetKeyPoints(string filePath)
+        {
+            Bitmap bitmap = new Bitmap(filePath);
+
+            Bitmap newBitmap = new Bitmap(bitmap);
+
+            Color[,] colors = GetColors(bitmap);
+        }
+
+        static Color[,] GetColors(Bitmap bitmap)
+        {
+            Color[,] colors = new Color[bitmap.Width, bitmap.Height];
+            for (int i = 0; i < bitmap.Width; i++)
+                for (int j = 0; j < bitmap.Height; j++)
+                    colors[i, j] = bitmap.GetPixel(i, j);
+            return colors;
+        }
+    }
+}
---
+++ Semester-4/Artificial-Intelligence-Systems/Graphipcs-and-Detectors/Graphipcs-and-Detectors/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Graphipcs_and_Detectors
+{
+    using System;
+    class Program
+    {
+        static void Main()
+        {
+            Graphics.GetKeyPoints(@"file.jpg");
+            Console.ReadKey();
+        }
+    }
+}
